ad161431cc2d67f9827c381645c09d7cfe721a9a
Antonio <dyachkov1984@inbox.ru>
2022-07-16 17:21:42 +0300

    Case 9

---
+++ 009/Program.cs
@@ -0,0 +1,12 @@
+﻿/*009. Вывести на экран четные числа от 1 до N
+*/
+int i=0;
+int N=20;
+while (i<=N)
+{
+    System.Console.Write($"{i} ");
+    i=i+2; 
+}
+//System.Console.WriteLine(123/10) это выбрасываем последнюю цифру
+//System.Console.WriteLine(123/10%) это добавитьб последнюю цифру
+//System.Console.Writeline(123/10%10) это получение числа
