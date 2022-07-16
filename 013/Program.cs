3140dfe086d49652b903347b02825eb51a17e748
Antonio <dyachkov1984@inbox.ru>
2022-07-16 17:25:25 +0300

    Case 13

---
+++ 013/Program.cs
@@ -0,0 +1,7 @@
+﻿/* Удалить вторую цифру целого числа введенного с клавиатуры.
+*/
+int a=Convert.ToInt32(Console.ReadLine());
+int b = a%10;
+int c = a/100;
+a = c*10+b;
+System.Console.WriteLine(a);
