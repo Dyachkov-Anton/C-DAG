ec6b651b6fdcf588a824dec45723d10c134db9fa
Antonio <dyachkov1984@inbox.ru>
2022-07-16 17:16:44 +0300

    Case 5

---
+++ 005/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+С клавиатуры вводятся три числа. 
+Найти максимальное из трех чисел
+*/
+double a,b,c;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+s=Console.ReadLine();
+c=Convert.ToDouble(s);
+if (a>b&a>c)
+{
+    System.Console.WriteLine(a);
+}  
+if (b>c&b>a)
+{
+    System.Console.WriteLine(b);
+}
+if (c>a&c>b)
+{
+    System.Console.WriteLine(c);
+}
