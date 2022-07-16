002a6f73db68dfce78c8100bfdda17d7a10268ef
Antonio <dyachkov1984@inbox.ru>
2022-07-16 17:14:59 +0300

    Case 3

---
+++ 003/Program.cs
@@ -0,0 +1,16 @@
+﻿/*С клавиатуры вводятся два числа a и b. 
+Найти максимальное из них
+*/
+double a,b;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+if (a>b)
+{
+    System.Console.WriteLine(a);
+}
+else 
+{
+    System.Console.WriteLine(b);
+}
