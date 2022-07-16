8c913da8c4cdcdf83343410c923b00944e5ed00c
Antonio <dyachkov1984@inbox.ru>
2022-07-16 17:13:33 +0300

    Case 2

---
+++ 002/Program.cs
@@ -0,0 +1,25 @@
+﻿/* С клавиатуры вводятся два вещественных числа. 
+Проверять является ли одно из них квадратом второго (блок-схема)
+*/
+double a,b;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+s=Console.ReadLine();
+b=Convert.ToDouble(s);
+if (a*a==b)
+{
+    System.Console.WriteLine("a is square b");
+}
+else 
+{
+    System.Console.WriteLine("a is not square b");
+}
+
+if (b*b==a)
+{
+    System.Console.WriteLine("b is square a");
+}
+else 
+{
+    System.Console.WriteLine("b is not square a");
+}
