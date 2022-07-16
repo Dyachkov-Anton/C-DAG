d50c9ef758bb826e8e2112d58825ebe7a32a7abf
Antonio <dyachkov1984@inbox.ru>
2022-07-16 17:20:33 +0300

    Case 8

---
+++ 008/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Вывести на экран числа от -N до N
+*/
+
+int n;
+int N;
+string? s=Console.ReadLine();
+n=Convert.ToInt32(s);
+s=Console.ReadLine();
+N=Convert.ToInt32(s);
+while (n<=N)
+{
+    System.Console.Write($"{n} ");
+    n=n+1; 
+}
