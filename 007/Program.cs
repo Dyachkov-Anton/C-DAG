73abed95007192a7787cabd2e2773586f8023791
Antonio <dyachkov1984@inbox.ru>
2022-07-16 17:19:46 +0300

    Case 7

---
+++ 007/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Выяснить является ли число чётным.
+*/
+double a;
+string? s=Console.ReadLine();
+a=Convert.ToDouble(s);
+if (a%2==0)
+{
+    System.Console.WriteLine($"{a} - even number!!!");
+}
+else 
+{
+    System.Console.WriteLine($"{a} - odd number!!!");
+}
