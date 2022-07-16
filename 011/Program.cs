699715b710e57da094a4abe177321d5a48e56c23
Antonio <dyachkov1984@inbox.ru>
2022-07-16 17:23:14 +0300

    Case 11

---
+++ 011/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Дано число больше 9. 
+Вывести на экран  вторую цифру числа с конца
+*/
+int number=Convert.ToInt32(Console.ReadLine());
+if (number>9)
+    {
+        Console.WriteLine(number.ToString()[1]);
+    }
+else
+{
+        Console.WriteLine("Не правильное число !!!");
+    }
