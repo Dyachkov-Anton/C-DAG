9710635077f60d949e81fb87aaa57ce114b2b1a3
Antonio <dyachkov1984@inbox.ru>
2022-07-18 20:39:03 +0300

    Case 21

---
+++ 021/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Задать номер четверти, показать диапазоны 
+для возможных координат
+*/
+double z=2;
+    if (z==1) 
+    {
+    System.Console.WriteLine("x>0 and y>0");
+    }
+    if (z==2) 
+    {
+        System.Console.WriteLine("x<0 and y>0");
+    }
+    if (z==3) 
+    {
+        System.Console.WriteLine("x<0 and y<0");
+    }
+    if (z==4) 
+    {System.Console.WriteLine("x>0 and y<0");
+    }
