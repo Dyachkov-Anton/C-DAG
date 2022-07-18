55f000c242b068075110f03fb3e7b867a334e306
Antonio <dyachkov1984@inbox.ru>
2022-07-18 21:40:49 +0300

    Case 30

---
+++ 030/Program.cs
@@ -0,0 +1,9 @@
+﻿/* 
+Написать программу вычисления произведения чисел от 1 до N
+*/
+int N = int.Parse(Console.ReadLine());
+int multiplication=1;
+    for (int i = 1; i <= N; i++)
+    multiplication*= i;
+Console.WriteLine(multiplication);
+Console.ReadKey();
