5f2149a841fa633e901f59f190f9f5cc8967ec16
Antonio <dyachkov1984@inbox.ru>
2022-07-18 21:20:51 +0300

    Case 28

---
+++ 028/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+Определить количество цифр в числе. Сделать подпрограмму.
+*/
+
+int Countdigits (int N)
+{
+    if (N==0) return 1;
+    int k = 0;
+    while (N!=0)
+    {
+        k++;
+        N/=10; 
+    }
+    return k;
+}
+System.Console.WriteLine(Countdigits(0));
