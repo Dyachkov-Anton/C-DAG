9632037a4b071b3665b9a27c6a318d611994f571
Antonio <dyachkov1984@inbox.ru>
2022-07-18 21:40:02 +0300

    Case 26

---
+++ 026/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+Найти сумму чисел от 1 до А
+*/
+int A = int.Parse(Console.ReadLine());
+int sum = 0;
+    for (int i = 1; i <= A; i++)
+    sum += i;
+Console.WriteLine(sum);
+Console.ReadKey();
