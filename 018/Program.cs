6e901d71b934917e46c58c97edb48c40f52977db
Antonio <dyachkov1984@inbox.ru>
2022-07-18 20:35:30 +0300

    Case 18

---
+++ 018/Program.cs
@@ -0,0 +1,19 @@
+﻿/*По двум заданным числам проверять является ли одно 
+квадратом другого. Используйте подпрограмму.
+*/
+
+bool Check (int a, int b)
+{
+    return a*a==b || b*b==a;
+}
+
+int a = 5;
+int b = 25;
+if (Check(a, b))
+    {
+        System.Console.WriteLine("Yes");
+    }
+else
+    {
+        System.Console.WriteLine("No");
+    } 
