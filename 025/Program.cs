f281064c3f2b0e9c2fa45a01a296d026bb9b775b
Antonio <dyachkov1984@inbox.ru>
2022-07-18 20:41:54 +0300

    Case 25

---
+++ 025/Program.cs
@@ -0,0 +1,12 @@
+﻿/* Вывести на экран кубы чисел от 1 до N
+*/
+Console.WriteLine("Введите число a = 1");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+    while (a <= N)
+        {
+            Console.WriteLine("{0} {1}", a, a * a* a);
+                a++;
+        }
+        Console.ReadKey();
