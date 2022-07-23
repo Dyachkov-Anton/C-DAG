8a2ebc952df1abf2c4505e1332f103de02e8f3cf
Antonio <dyachkov1984@inbox.ru>
2022-07-23 23:17:06 +0300

    Case 31

---
+++ 031/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Показать кубы чисел, заканчивающихся на четную цифру
+*/
+Console.WriteLine("Введите число a равное 2");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine());
+    while (a <= N)
+        {
+            Console.WriteLine("{0} {1}", a, a * a* a);
+                a=a+2;
+        }
+        Console.ReadKey();
