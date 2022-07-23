bd5cfafd4e3e1dc54bab847f1e6567c665b5aefb
Antonio <dyachkov1984@inbox.ru>
2022-07-23 23:21:15 +0300

    Case 35

---
+++ 035/Program.cs
@@ -0,0 +1,29 @@
+﻿/* 
+Написать программу замены элементов массива на противоположные
+*/
+/*int N=3;
+int [] a=new int[N];
+Random random=new Random();
+for (int i=0; i<a.Length; i++)
+    a[i]=random.Next(-5, 5);
+for (int i=0; i<a.Length; i++)
+    System.Console.Write($"{a[i],4}");
+    System.Console.WriteLine();
+*/
+int[] a = new int[3];
+            Random r = new Random();
+
+            Console.WriteLine("Исходный массив: ");
+            for (int i = 0; i<a.Length;i++)
+            {
+                a[i] = r.Next(-5, 5);
+                Console.Write(a[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("Измененный массив: ");
+            for (int i =0; i < a.Length; i++)
+            {
+                a[i] = a[i] * (-1);
+                Console.Write(a[i] + " ");
+            }
+            Console.ReadKey();
