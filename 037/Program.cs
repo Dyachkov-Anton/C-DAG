618d1ed13d289212ecedffb1a041998a8d5f8122
Antonio <dyachkov1984@inbox.ru>
2022-07-23 23:22:36 +0300

    Case 37

---
+++ 037/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Задать массив, заполнить случайными положительными 
+трёхзначными числами. Показать количество нечетных\четных чисел
+*/
+int N=5;
+int [] a=new int[N];
+Random random=new Random();
+for (int i=0; i<a.Length; i++)
+{
+    a[i]=random.Next(100,999);
+}
+
+int evenNumber=0;
+int oddNumber=0;
+for (int i=0; i<a.Length; i++)
+    {
+        if (a[i]%2==0)
+        {
+            ++evenNumber;
+        }
+        else
+        ++oddNumber;
+    }   
+for (int i=0; i<a.Length; i++)
+    System.Console.Write($"{a[i],4}");
+    System.Console.WriteLine();
+System.Console.WriteLine($"Even Number:{evenNumber}");
+System.Console.WriteLine($"Odd Number:{oddNumber}");
