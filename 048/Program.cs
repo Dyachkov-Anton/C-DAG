86690c2ad88c34989bc31a5234b7e776b064c12b
Antonio <dyachkov1984@inbox.ru>
2022-08-10 19:13:02 +0300

    Case 48

---
+++ 048/Program.cs
@@ -0,0 +1,20 @@
+﻿/*
+__Написать программу копирования массива
+*/
+double[] a = new double[10];
+double aCopy;
+
+Console.Write("Оригинал ");
+for (int i = 0; i < a.Length; i++)
+{
+    a[i] = new Random().Next(0, 100);
+    Console.Write(a[i] + " ");
+}
+Console.WriteLine();
+Console.Write("   Копия ");
+for (int i = 0; i < a.Length; i++)
+{
+    aCopy = a[i];
+    Console.Write(aCopy + " ");
+}
+Console.WriteLine();
