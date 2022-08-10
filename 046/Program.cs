0c29960c6a86c22022671a13cefa1a8fd6381522
Antonio <dyachkov1984@inbox.ru>
2022-08-10 19:09:56 +0300

    Case 46

---
+++ 046/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+С клавиатуры вводится число N. Показать первые N чисел Фибоначчи. 
+Принять первые числа равными 0 и 1
+*/
+int N =int.Parse(Console.ReadLine());
+int f1 = 0;
+int f2 = 1;
+int result;
+for (int i = 0; i < N; i++)
+{
+    result = f1 + f2;
+    Console.Write(result + " ");
+    f1 = f2;
+    f2 = result;
+}
