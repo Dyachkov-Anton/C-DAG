90ee76da2d857b8ff4f6f21780cf19a17f79fea2
Antonio <dyachkov1984@inbox.ru>
2022-08-10 19:06:28 +0300

    Case 42

---
+++ 042/Program.cs
@@ -0,0 +1,15 @@
+﻿/*
+Выяснить являются ли три числа сторонами треугольника
+*/
+Random rand = new Random();
+int a = rand.Next(1, 50);
+Console.WriteLine("Число а: " + a);
+int b = rand.Next(1, 50);
+Console.WriteLine("Число b: " + b);
+int c = rand.Next(1, 50);
+Console.WriteLine("Число c: " + c);
+
+if ((a + b > c) && (a + c > b) && (b + c > a)) 
+Console.WriteLine("Числа являются сторонами треугольника");
+else 
+Console.WriteLine("Числа не являются сторонами треугольника");
