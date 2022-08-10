168b2cddda1d4a49e30a58fb726297881b5ead1b
Antonio <dyachkov1984@inbox.ru>
2022-08-10 19:08:29 +0300

    Case 44

---
+++ 044/Program.cs
@@ -0,0 +1,14 @@
+﻿/*
+Написать программу преобразования десятичного числа в двоичное
+*/
+Console.WriteLine("Введите число: ");
+int a = int.Parse(Console.ReadLine());
+int k;
+string result = " ";
+while (a > 0)
+{
+    k = a % 2;
+    result = k + result;
+    a = a / 2;
+}
+Console.WriteLine($"Вывод: {result}");
