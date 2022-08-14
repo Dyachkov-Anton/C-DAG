3970f3f99122aa425e7b63bc03d86b68ea312f15
Antonio <dyachkov1984@inbox.ru>
2022-08-14 19:29:53 +0300

    Case 51

---
+++ 051/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Создать подпрограмму Create2DArray(m,n), 
+которая возвращает двумерный массив следующим правилом: Aij = i+j, 
+где i и j - индексы элементов массива, а m и n - размеры массива
+*/
+
+int[,] arr;//объявляем массив
+
+arr=Create2DArray(3,4);
+Print(arr);
+
+void Print(int[,] a)
+{
+    for(int i=0;i<a.GetLength(0);i++)//перебираем строки
+    {
+            for(int j=0;j<a.GetLength(1);j++)//перебираем столбцы
+                System.Console.Write($"{a[i,j],4}");
+            System.Console.WriteLine();
+    }
+}
+
+int[,] Create2DArray(int n,int m)
+{
+    int[,] a=new int[n,m];//создаем массив
+    Random random=new Random();
+    for(int i=0;i<a.GetLength(0);i++)//перебираем строки
+        for(int j=0;j<a.GetLength(1);j++)//перебираем столбцы
+            a[i,j]=i+j;
+    return a;
+}
