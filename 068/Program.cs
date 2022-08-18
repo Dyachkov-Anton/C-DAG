385551f1cc3b5c27ae31e37b9feca0d36367eefe
Antonio <dyachkov1984@inbox.ru>
2022-08-18 17:57:01 +0300

    Case 68

---
+++ 068/Program.cs
@@ -0,0 +1,10 @@
+﻿/*
+Показать натуральные числа от M до N, N и M заданы
+*/
+void PrintNumber(int M, int N)
+{
+    if (N < M) return;
+    PrintNumber(M, N - 1);
+    Console.Write(N + " ");
+}
+PrintNumber(10,20);
