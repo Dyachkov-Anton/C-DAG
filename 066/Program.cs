fc57d3ef462e1475099a0a0d4d180ce96df23b26
Antonio <dyachkov1984@inbox.ru>
2022-08-18 17:55:27 +0300

    Case 66

---
+++ 066/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Показать натуральные числа от 1 до N, N задано
+*/
+
+void PrintNumber(int N)
+{
+    if (N < 1) return;
+    PrintNumber(N - 1);
+    Console.Write(N + " ");
+}
+PrintNumber(100);
+
