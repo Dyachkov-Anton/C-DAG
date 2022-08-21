62e1da5e70be93696bde2f3dacce1597f7714616
Antonio <dyachkov1984@inbox.ru>
2022-08-21 17:37:58 +0300

    Case string5

---
+++ string5/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+ Определить являются ли введенные с клавиатуры символы правильно 
+ записью целого числа. 
+ Вычислить сумму цифр введенного числа
+ */
+using System;
+namespace SharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args) {
+            string num = Console.ReadLine();
+            int temp = 0;
+            if (!Int32.TryParse(num, out temp))
+                Console.WriteLine("Это не является верной записью целого");
+            else {
+                int sum = 0;
+                foreach (char digit in num)
+                    sum += digit - 48;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
