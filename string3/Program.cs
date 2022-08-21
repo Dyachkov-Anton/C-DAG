f1cc4f51c9a334dd85b0baa7188741cf8ae2ee18
Antonio <dyachkov1984@inbox.ru>
2022-08-21 17:35:27 +0300

    Case string3

---
+++ string3/Program.cs
@@ -0,0 +1,8 @@
+﻿/*
+C клавиатуры вводится строка разделенная точкой. 
+Подсчитать количество символов до точки
+*/
+Console.WriteLine("Do string");
+string str = Console.ReadLine();
+int countBefore = str.LastIndexOf('.');
+Console.WriteLine(countBefore);
